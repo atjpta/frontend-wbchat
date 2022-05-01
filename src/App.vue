a1c167631c158ff4269119a5a4cc1025efecdf47
atjpta <caoducan351@gmail.com>
2022-05-01 12:55:49 +0700

    deploy

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Vheader from '@/components/vheader.vue';
+
+</script>
+
+<template>
+  <div class=" min-h-screen" style=" background-repeat: unset; background-image: url(https://get.wallhere.com/photo/sunlight-anime-planet-water-reflection-sky-stars-clouds-Earth-blue-moonlight-horizon-atmosphere-cloud-ocean-wave-computer-wallpaper-atmosphere-of-earth-outer-space-wind-wave-meteorological-phenomenon-208522.jpg)">
+   <Vheader />
+   <router-view />   
+  </div>
+</template>
+
---
+++ src/services/user.service.js
@@ -0,0 +1,28 @@
+import { createApiClient } from "./api.service";
+
+class UserService {
+    constructor(baseUrl = "/api/users") {
+		this.api = createApiClient(baseUrl, true);
+	}
+    async getAllUser() {
+        return (await this.api.get("/")).data;
+    }
+    async create(data) {
+        return (await this.api.post("/", data)).data;
+    }
+    async get(id) {
+        return (await this.api.get(`/${id}`)).data;
+    }
+    async update(id, data) {
+        return (await this.api.put(`/${id}`, data)).data;
+    }
+
+    async testuser() {
+        return (await this.api.get(`/test/user`)).data;
+    }
+
+    async testuser() {
+        return (await this.http.get(`/test/admin`)).data;
+    }
+}
+export default new UserService();
